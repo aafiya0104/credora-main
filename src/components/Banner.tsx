b99b8419d8efc60464f6d07eac0382eb601cd018
Lenovo <aafiyahussain9@gmail.com>
2025-04-20 02:11:05 +0530

    Initial commit: set up project structure and navbar component

---
+++ src/components/Banner.tsx
@@ -0,0 +1,12 @@
+export const Banner = () => {
+  return (
+    <div className="py-3 text-center" style={{backgroundImage: "linear-gradient(to right, rgba(252, 214, 255, 0.7), rgba(41, 216, 255, 0.7), rgba(255, 253, 128, 0.7), rgba(248, 154, 191, 0.7), rgba(252, 214, 255, 0.7))"}}>
+      <div className="container">
+       <p className="font-medium">
+        <span className="hidden sm:inline">Introducing a completely redesigned interface - </span><a href="#" className="underline underline-offset-4 font-medium">
+          Explore The Demo
+        </a></p>
+      </div>
+    </div>
+  );
+};
---
+++ src/components/Navbar.tsx
@@ -0,0 +1,28 @@
+import React from 'react';
+import logoImage from "../assets/images/logosaas.png";
+import Image from 'next/image';
+import MenuIcon from "../assets/icons/menu.svg";
+
+export const Navbar = () => {
+  return (
+    <div className="container bg-black">
+      <div className="py-4 flex items-center justify-between">
+        <div className="relative">
+          <div className="absolute w-full top-2 bottom-0 bg-[linear-gradient(to-right, rgba(252, 214, 255, 0.7), rgba(41, 216, 255, 0.7), rgba(255, 253, 128, 0.7), rgba(248, 154, 191, 0.7), rgba(252, 214, 255, 0.7)] blur-md"></div>
+          <Image src={logoImage} alt="SaaS Logo" className="h-12 w-12 relative"/>  
+        </div>
+        <div className="border border-white border-opacity-30 h-10 w-10 flex justify-center items-center rounded-lg sm:hidden">
+          <MenuIcon className="text-white" />
+        </div>
+        <nav className="text-white flex gap-6">
+          <a href="#" className="text-opacity-60 text-white hover:text-opacity-100 transition">About</a>
+          <a href="#" className="text-opacity-60 text-white hover:text-opacity-100 transition">Features</a>
+          <a href="#" className="text-opacity-60 text-white hover:text-opacity-100 transition">Updates</a>
+          <a href="#" className="text-opacity-60 text-white hover:text-opacity-100 transition">Help</a>
+          <a href="#" className="text-opacity-60 text-white hover:text-opacity-100 transition">Customers</a>
+          <button>Get for free</button>
+        </nav>
+      </div>
+    </div>
+  );
+};
