c90465ce0516ff2deccf9a46368fc3bf7400e344
Lenovo <aafiyahussain9@gmail.com>
2025-04-20 03:27:16 +0530

    updating landing page

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -5,7 +5,8 @@ import MenuIcon from "../assets/icons/menu.svg";
 
 export const Navbar = () => {
   return (
-    <div className="container bg-black">
+    <div className=" bg-black">
+    <div className="px-4">
       <div className="py-4 flex items-center justify-between">
         <div className="relative">
           <div className="absolute w-full top-2 bottom-0 bg-[linear-gradient(to-right, rgba(252, 214, 255, 0.7), rgba(41, 216, 255, 0.7), rgba(255, 253, 128, 0.7), rgba(248, 154, 191, 0.7), rgba(252, 214, 255, 0.7)] blur-md"></div>
@@ -14,15 +15,17 @@ export const Navbar = () => {
         <div className="border border-white border-opacity-30 h-10 w-10 flex justify-center items-center rounded-lg sm:hidden">
           <MenuIcon className="text-white" />
         </div>
-        <nav className="text-white flex gap-6">
+        <nav className="text-white gap-6 items-center hidden sm:flex">
           <a href="#" className="text-opacity-60 text-white hover:text-opacity-100 transition">About</a>
           <a href="#" className="text-opacity-60 text-white hover:text-opacity-100 transition">Features</a>
           <a href="#" className="text-opacity-60 text-white hover:text-opacity-100 transition">Updates</a>
           <a href="#" className="text-opacity-60 text-white hover:text-opacity-100 transition">Help</a>
           <a href="#" className="text-opacity-60 text-white hover:text-opacity-100 transition">Customers</a>
-          <button>Get for free</button>
+
+          <button className="bg-white text-black py-2 px-4 rounded-lg">Get for free</button>
         </nav>
       </div>
+    </div>
     </div>
   );
 };
